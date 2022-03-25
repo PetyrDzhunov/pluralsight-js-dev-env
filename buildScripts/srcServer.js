4bd8e9644535158a109cd02b02a62512ed911d13
Petar Dzhunov <petar.djunov@abv.bg>
2022-03-25 18:34:34 +0200

    add webpack as a bundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import { join } from 'path';
 import open from 'open';
+import config from '../webpack.config.dev';
+import webpack from 'webpack';
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require('webpack-dev-middleware')(compiler, {
+    publicPath: config.output.publicPath,
+  }),
+);
 
 app.get('/', function (req, res) {
   res.sendFile(join(__dirname, '../src/index.html'));
